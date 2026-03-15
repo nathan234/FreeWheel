be400f7c175482293f5394c5bfaa1838cc86bafe
Nathan Retta <nretta@gmail.com>
2026-03-15 12:23:27 -0600

    Revert "Fix splash screen icon: remove off-white border, use black background"
    This reverts commit 25449e42caabbf641ddceab451f2f71d5c0e1a67.

--- iosApp/FreeWheel/Views/SplashView.swift
+++ iosApp/FreeWheel/Views/SplashView.swift
@@ -3,13 +3,14 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         ZStack {
-            Color.black
+            Color(uiColor: .systemBackground)
                 .ignoresSafeArea()
 
             Image("SplashLogo")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 120, height: 120)
+                .clipShape(RoundedRectangle(cornerRadius: 27, style: .continuous))
         }
     }
 }
